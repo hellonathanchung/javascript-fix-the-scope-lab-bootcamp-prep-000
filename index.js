129f0ffbed9c912b16095abfd41556f0d74a5a19
Nathan Chung <chung.nathanh@gmail.com>
2020-05-27 03:59:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
   var animal = yourAnimal()
-  return (`${animal}`);
+  return yourAnimal(cat)
 }
 
 function add2(n) {
